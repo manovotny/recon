82d5aaf0ab351883f379b15f1f6f92ea7955843c
Michael Novotny <manovotny@gmail.com>
2015-12-30 23:41:13 -0600

    Overhauls webpack configurations.

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,11 +1,9 @@
-/* eslint strict: 0 */
-'use strict';
-
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
-const baseConfig = require('./webpack.config.base');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const baseConfig = require('./webpack.config.base');
 
 const config = Object.create(baseConfig);
 
@@ -15,36 +13,32 @@ config.entry = './app/index';
 
 config.output.publicPath = '/dist/';
 
-config.module.loaders.push({
-  test: /^((?!\.module).)*\.css$/,
-  loader: ExtractTextPlugin.extract(
-    'style-loader',
-    'css-loader'
-  )
-}, {
-  test: /\.module\.css$/,
-  loader: ExtractTextPlugin.extract(
-    'style-loader',
-    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-  )
-});
-
 config.plugins.push(
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    '__DEV__': false,
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      screw_ie8: true,
-      warnings: false
-    }
-  }),
-  new ExtractTextPlugin('style.css', { allChunks: true })
+    new webpack.DefinePlugin({
+        '__DEV__': false,
+        'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+        }
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    // TODO: Delete when CSS is removed from boilerplate example.
+    new ExtractTextPlugin('style.css', {allChunks: true})
 );
+
+// TODO: Delete when CSS is removed from boilerplate example.
+config.module.loaders.push({
+    test: /^((?!\.module).)*\.css$/,
+    loader: ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader'
+    )
+}, {
+    test: /\.module\.css$/,
+    loader: ExtractTextPlugin.extract(
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+    )
+});
 
 config.target = webpackTargetElectronRenderer(config);
 
