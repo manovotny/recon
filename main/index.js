986128e50c65ecb6fd084dca32dfe2a19d2ac3cf
Michael Novotny <manovotny@gmail.com>
2017-09-28 14:16:15 -0500

    Adds Chrome extension devtools.

--- main/index.js
+++ main/index.js
@@ -8,8 +8,25 @@ const electronWindowState = require('electron-window-state');
 
 require('electron-debug')({showDevTools: isDev});
 
+const installExtensions = () => {
+    const {default: installExtension, REACT_DEVELOPER_TOOLS, REACT_PERF, REDUX_DEVTOOLS} = require('electron-devtools-installer');
+    const extensions = [
+        REACT_DEVELOPER_TOOLS,
+        REACT_PERF,
+        REDUX_DEVTOOLS
+    ];
+
+    extensions.forEach((extension) => {
+        installExtension(extension)
+            .then((name) => console.log(`Added Extension: ${name}`))
+            .catch((error) => console.log('An error occurred: ', error));
+    });
+};
+
 app.on('ready', async () => {
     await prepareNext('./renderer');
+
+    installExtensions();
 
     const windowState = electronWindowState();
 
